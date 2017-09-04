529a438e41176ae85079075999bb52411a2a97d5
Kevin Cazelles <kcazelle@uoguelph.ca>
2017-09-04 13:52:34 -0400

    re-add AnimationinR'

---
+++ pkgsList.R
@@ -0,0 +1,27 @@
+# On pourrait faire utiliser un curl à la place sur le gist, du genre
+# curl -L https://gist.githubusercontent.com/KevCaz/aaa83151e9d12cc07fbe379e2c32a385/raw/35a76e7cc192ba5fbc5a761025da87705893a215/letiRWSPkgs.R > pkgsList.R
+## CRAN packages
+install.packages(
+  c(
+    "animation",
+    "biomod2",
+    "devtools",
+    "kableExtra",
+    "leaflet",
+    "plyr",
+    "rmarkdown",
+    "sdmpredictors",
+    "sf",
+    "tm",
+    "showtext",
+    "wordcloud2",
+    "webshot",
+    "yaml"
+  ),
+  repos = "https://cran.wu.ac.at/",
+  dependencies = TRUE
+)
+
+## Github packages
+devtools::install_github("iobis/robis")
+devtools::install_github("KevCaz/graphicsutils")
