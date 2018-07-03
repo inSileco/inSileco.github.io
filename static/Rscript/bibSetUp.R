bf3935a0fad2fe839f85d2598b3f1e1154401584
David Beauchesne <david.beauchesne@uqar.ca>
2018-07-03 10:53:21 -0400

    repush latest changes

--- static/Rscript/bibSetUp.R
+++ static/Rscript/bibSetUp.R
@@ -1,8 +1,10 @@
 bibFile <- 'inSilecoRef.bib'
 bibPath <- 'static/bib/'
+
 while (file.exists(paste0(bibPath,bibFile)) == F) {
   bibPath <- paste0('../', bibPath)
 }
+
 listDOI <- paste0(bibPath, 'listDOI.csv')
 bibFile <- paste0(bibPath, bibFile)
 DOI <- read.csv(listDOI, stringsAsFactors = F)
