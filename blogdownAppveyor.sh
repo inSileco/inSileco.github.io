0879cf8378ba57bf225e1f29570cd1a21ea146c9
Kevin Cazelles <kcazelle@uoguelph.ca>
2018-07-31 09:20:39 -0400

    use wget in appveyor

--- blogdownAppveyor.sh
+++ blogdownAppveyor.sh
@@ -6,15 +6,17 @@ PANDOC_DIR="${HOME}/opt/pandoc"
 PANDOC_URL="https://github.com/jgm/pandoc/releases/download/2.2.2.1/pandoc-2.2.2.1-windows-x86_64.zip"
 
 local os_path="$1"
-mkdir -p "${PANDOC_DIR}"
-curl -o /tmp/pandoc-${PANDOC_VERSION}.zip ${PANDOC_URL}
+wget https://github.com/jgm/pandoc/releases/download/2.1.3/pandoc-2.1.3-1-amd64.deb -O /tmp/pandoc-${PANDOC_VERSION}.zip
+
+
+# mkdir -p "${PANDOC_DIR}"
+# curl -o /tmp/pandoc-${PANDOC_VERSION}.zip ${PANDOC_URL}
 unzip -j /tmp/pandoc-${PANDOC_VERSION}.zip "pandoc-${PANDOC_VERSION}/${os_path}/pandoc" -d "${PANDOC_DIR}"
 chmod +x "${PANDOC_DIR}/pandoc"
 sudo ln -s "${PANDOC_DIR}/pandoc" /usr/local/bin
 unzip -j /tmp/pandoc-${PANDOC_VERSION}.zip "pandoc-${PANDOC_VERSION}/${os_path}/pandoc-citeproc" -d "${PANDOC_DIR}"
 chmod +x "${PANDOC_DIR}/pandoc-citeproc"
 sudo ln -s "${PANDOC_DIR}/pandoc-citeproc" /usr/local/bin
-
 
 ##
 Rscript -e 'blogdown::install_hugo()'
