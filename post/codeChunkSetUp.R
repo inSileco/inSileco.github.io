a55cbeb9d896f885b6eb1a2ef73aec597c575129
KevCaz <KevCaz@users.noreply.github.com>
2021-04-10 22:05:47 +0000

    deploy: 6af392682a4f5007bd2b7e970fc8618d83496998

---
+++ post/codeChunkSetUp.R
@@ -0,0 +1,11 @@
+knitr::opts_chunk$set(
+  comment = "#R>",
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  #fig.width = 7.5,
+  #fig.height = 6,
+  fig.align = 'center',
+  width = 100
+)
+library(inSilecoRef)
---
+++ tips/codeChunkSetUp.R
@@ -0,0 +1,10 @@
+knitr::opts_chunk$set(
+  comment = "#R>",
+  collapse = TRUE,
+  warning = FALSE,
+  message = FALSE,
+  #fig.width = 7.5,
+  #fig.height = 6,
+  fig.align = 'center',
+  width = 120
+)
